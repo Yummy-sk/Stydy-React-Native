f68328d3004251bb11e6575b19e820dbc241e163
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-01-07 20:19:40 -0500

    chater 03

---
+++ TodoApp/components/AddTodo.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+  Keyboard,
+} from 'react-native';
+import image from '../assets/icons/add_white/add_white.png';
+
+export default function AddTodo() {
+  const [text, setText] = useState('');
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={image} />
+    </View>
+  );
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할일을 입력하세요"
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
+      />
+      {Platform.select({
+        ios: (
+          <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+            {button}
+          </TouchableOpacity>
+        ),
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    height: 64,
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+  },
+});
---
+++ TodoApp/components/DateHead.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+export default function DateHead({date}) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const {top} = useSafeAreaInsets();
+
+  return (
+    <>
+      <View style={[styles.statusBarPlaceholder, {height: top}]} />
+      <StatusBar barStyle="light-content" />
+      <View style={styles.block}>
+        <Text style={styles.dateText}>
+          {year}년 {month}월 {day}일
+        </Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  statusBarPlaceholder: {
+    backgroundColor: '#26a69a',
+  },
+  block: {
+    padding: 16,
+    backgroundColor: '#26a29a',
+  },
+  dateText: {
+    fontSize: 14,
+    color: 'white',
+  },
+});
